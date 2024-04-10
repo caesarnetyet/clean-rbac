4cdca217e8919f3f4e4b5da3088f9a30d6110142
Julio Cesar Tovar <caesarwenli@gmail.com>
2024-04-09 19:24:12 -0600

    fix roles

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    apiUrl: 'https://netyet.tech/api',
+    apiUrl: window.location.origin.startsWith('http://10.8.0.') ? 'http://10.8.0.2/api' : 'https://netyet.tech/api',
     siteKey: '6LewVZ0pAAAAAD583Mc0C8msCWOPjVHI4nAmgyGC',
     production: true
 };
